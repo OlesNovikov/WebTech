a37e6c2b66bb92860152d7dacf386e962896de47
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-04-14 16:48:26 +0300

    Add files via upload
    в произвольном тексте все номера телефонов (предусмотреть не менее пяти вариантов записи номера) вывести зелёным цветом. При этом номера сотовых телефонов (начинаются с "+КОД-") подчеркнуть. Текст вводить через форму.

---
+++ navigation.php
@@ -0,0 +1,6 @@
+<li><a class="<?php echo basename($_SERVER["PHP_SELF"]) == 'index.php' ? "active" : "normal"; ?>" href="index.php">Дамашняя</a></li>
+<li><a class="<?php echo basename($_SERVER["PHP_SELF"]) == 'SIGHTS.php' ? "active" : "normal"; ?>" href="SIGHTS.php">Славутасці</a></li>
+<li><a class="<?php echo basename($_SERVER["PHP_SELF"]) == 'MUSEUMS.php' ? "active" : "normal"; ?>" href="MUSEUMS.php">Музеі</a></li>
+<li><a class="<?php echo basename($_SERVER["PHP_SELF"]) == 'FOOD.php' ? "active" : "normal"; ?>" href="FOOD.php">Кухня</a></li>
+<li><a class="<?php echo basename($_SERVER["PHP_SELF"]) == 'HOTELS.php' ? "active" : "normal"; ?>" href="HOTELS.php">Жыллё</a></li>
+<li><a class="<?php echo basename($_SERVER["PHP_SELF"]) == 'AdditionalTask.php' ? "active" : "normal"; ?>" href="AdditionalTask.php">Дадатковае заданне</a></li>
