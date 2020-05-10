efb1686d8a51c7534652f46a1ac340f1bffa9a9c
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-05-10 21:38:34 +0300

    Update dbConnect.php

--- dbConnect.php
+++ dbConnect.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
-    $user = 'Oles';
-    $password = 'Erda_2020';
+    $user = 'user';
+    $password = 'password';
     $dbName = 'minskguide';
     $link = mysqli_connect($host, $user, $password, $dbName);
 ?>
