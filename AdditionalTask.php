9720b6666b27da8b600d0ae311b2bfac3c0a328b
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-05-10 21:33:42 +0300

    Update AdditionalTask.php

--- AdditionalTask.php
+++ AdditionalTask.php
@@ -265,8 +265,8 @@
                         if (yearInRange()) {
                             $year = $_POST['input_year'];
                             $host = 'localhost';
-                            $user = 'Oles';
-                            $password = 'Erda_2020';
+                            $user = 'user';
+                            $password = 'password';
                             $dbName = 'minskguide';
 
                             $link = mysqli_connect($host, $user, $password, $dbName);
